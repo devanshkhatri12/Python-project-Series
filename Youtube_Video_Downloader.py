be1dfb372913218f1c6cb31a68f0912c5a711838
devanshkhatri12 <dkkhatri941@gmail.com>
2024-06-28 22:55:11 +0530

    Optimized Code

--- Youtube_Video_Downloader.py
+++ Youtube_Video_Downloader.py
@@ -12,9 +12,23 @@ def download_video(url, save_path):
     except Exception as e:
         print(e)
 
+def open_file_dialog():
+    folder = filedialog.askdirectory()
+    if folder:
+        print(f"Selected folder: {folder}")
 
-url = "https://www.youtube.com/watch?v=RrgsrLo00kE"
-save_path = "F:\c++\Documents\Writing Practice"
+    return folder
 
-download_video(url, save_path)
+if __name__ == "__main__":
+    root = tk.Tk()                  #initialized tk module open tk() window
+    root.withdraw()                 # windohide the window
+
+    video_url = input("Please enter a YouTube url: ")
+    save_dir = open_file_dialog()
+
+    if save_dir:
+        print("Started dowload...")
+        download_video(video_url, save_dir)
+    else:
+        print("Invalid save location. ")
 
